5c7bc84f3e53f0cf63ce62a1b0d5879051cf664c
bcifu <ben.cifu@yale.edu>
2023-10-07 00:45:25 +0000

    Switch to writev for print

--- src/stdio/__stdio_write.c
+++ src/stdio/__stdio_write.c
@@ -16,16 +16,58 @@
 size_t __stdio_write(FILE *f, const unsigned char *buf, size_t len)
 {
 	#ifndef _CERTIKOS_
-	struct iovec iovs[2] = {
+		struct iovec iovs[2] = {
 		{ .iov_base = f->wbase, .iov_len = f->wpos-f->wbase },
 		{ .iov_base = (void *)buf, .iov_len = len }
 	};
+	#else 
+		struct ringleader* rl = get_ringleader();
+		void* shmem = get_rl_shmem();
+		struct iovec *iovs = (struct iovec *) shmem;
+
+		size_t block_two_offset = len;
+
+		iovs[0] = (struct iovec) {.iov_base = &iovs[2] + block_two_offset, .iov_len = f->wpos-f->wbase};
+		iovs[1] = (struct iovec) {.iov_base = &iovs[2], len};
+
+		//copy past where the iovecs live
+		memcpy(&iovs[2], (void *) buf, len);
+		memcpy(&iovs[2] + block_two_offset, f->wbase, f->wpos-f->wbase);
+	#endif
+	
 	struct iovec *iov = iovs;
 	size_t rem = iov[0].iov_len + iov[1].iov_len;
 	int iovcnt = 2;
 	ssize_t cnt;
+
+	//TODO figure out this loops logic
 	for (;;) {
-		cnt = syscall(SYS_writev, f->fd, iov, iovcnt);
+
+		#ifndef _CERTIKOS_
+			cnt = syscall(SYS_writev, f->fd, iov, iovcnt);
+		#else
+			int32_t id = ringleader_prep_writev(rl, f->fd, iov, iovcnt, 0);
+			ringleader_set_user_data(rl, id, (void *) WRITE_COOKIE);
+			ringleader_submit(rl);
+			syscall(SYS_sched_yield);
+
+			struct io_uring_cqe *cqe;
+			while(1) {
+			cqe = ringleader_peek_cqe(rl);
+			if(cqe != NULL){
+				if((uint64_t) cqe->user_data == WRITE_COOKIE){
+					__s32 ret = cqe->res;
+					ringleader_consume_cqe(rl, cqe);
+					cnt = ret;
+					break;
+				} else {
+					certikos_puts("Did not get expected ringleader write completion token");
+					cnt = 0;
+					break;
+				}
+			}}
+		#endif
+		
 		if (cnt == rem) {
 			f->wend = f->buf + f->buf_size;
 			f->wpos = f->wbase = f->buf;
@@ -41,35 +83,9 @@ size_t __stdio_write(FILE *f, const unsigned char *buf, size_t len)
 			cnt -= iov[0].iov_len;
 			iov++; iovcnt--;
 		}
+
+		//I NEED TO FIX THIS UPDATE THING AND DEAL WITH THAT I BONK THE IOV
 		iov[0].iov_base = (char *)iov[0].iov_base + cnt;
 		iov[0].iov_len -= cnt;
 	}
-	#else
-		//using write for now
-		struct ringleader* rl = get_ringleader();
-		void* shmem = get_rl_shmem();
-		//get shmem
-		memcpy(shmem, f->wbase, f->wpos-f->wbase);
-		memcpy((char *)shmem + (f->wpos - f->wbase), buf, len);
-		int32_t id = ringleader_prep_write(rl, f->fd, shmem, len + (size_t) (f->wpos - f->wbase), 0);
-        ringleader_set_user_data(rl, id, (void *) WRITE_COOKIE);
-        ringleader_submit(rl);
-		syscall(SYS_sched_yield);
-
-		struct io_uring_cqe *cqe;
-		while(1) {
-        cqe = ringleader_peek_cqe(rl);
-        if(cqe != NULL){
-			if((uint64_t) cqe->user_data == WRITE_COOKIE){
-				__s32 ret = cqe->res;
-				ringleader_consume_cqe(rl, cqe);
-				return ret;
-			} else {
-				certikos_puts("Did not get expected ringleader write completion token");
-                exit(-1);
-				return 0;
-			}
-		}}
-		
-#endif
 }
