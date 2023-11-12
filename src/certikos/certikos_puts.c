a651f668c19703b1137010ad9e28ac46b92fe632
bcifu <ben.cifu@yale.edu>
2023-11-12 00:03:24 +0000

    Socket

--- src/certikos/certikos_puts.c
+++ src/certikos/certikos_puts.c
@@ -2,7 +2,6 @@
 #include "certikos_impl.h"
 #include <string.h>
 
-//TODO: replace magic 0
 int certikos_puts(const char* buf){
     int len = strlen(buf);
     return syscall(SYS_certikos_puts, DEFAULT_CONSOLE_ID, buf, len);
