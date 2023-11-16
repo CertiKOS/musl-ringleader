398752ea498ef74faeadf658ee903e8d5d79f62c
Richard Habeeb <richard.habeeb@gmail.com>
2023-11-15 23:01:25 -0500

    container_of

--- include/certikos.h
+++ include/certikos.h
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <ringleader.h>
 
+/* Return the offset of 'member' relative to the beginning of a struct type */
+#define offsetof(type, member)  __builtin_offsetof(type, member)
+
+#define container_of(ptr, type, member)                       \
+    ({                                                        \
+        const __typeof__(((type*)0)->member)* __mptr = (ptr); \
+        (type*)((char*)__mptr - offsetof(type, member));      \
+    })
 
 int sys_io_uring_setup(uint32_t entries, struct io_uring_params *p);
 
