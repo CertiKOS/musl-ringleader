0e461ee4382d4399e2879b6024612ccbbe1139c1
bcifu <ben.cifu@yale.edu>
2023-11-24 03:20:04 +0000

    Read, fadvise

--- src/fcntl/posix_fadvise.c
+++ src/fcntl/posix_fadvise.c
@@ -1,16 +1,43 @@
 #include <fcntl.h>
 #include "syscall.h"
 
+#ifdef _CERTIKOS_
+#include "certikos_impl.h"
+#include "ringleader.h"
+#define ADVISE_COOKIE (2241254)
+#endif
+
 int posix_fadvise(int fd, off_t base, off_t len, int advice)
 {
-#if defined(SYSCALL_FADVISE_6_ARG)
-	/* Some archs, at least arm and powerpc, have the syscall
-	 * arguments reordered to avoid needing 7 argument registers
-	 * due to 64-bit argument alignment. */
-	return -__syscall(SYS_fadvise, fd, advice,
-		__SYSCALL_LL_E(base), __SYSCALL_LL_E(len));
-#else
-	return -__syscall(SYS_fadvise, fd, __SYSCALL_LL_O(base),
-		__SYSCALL_LL_E(len), advice);
-#endif
+	#ifndef _CERTIKOS_
+	#if defined(SYSCALL_FADVISE_6_ARG)
+		/* Some archs, at least arm and powerpc, have the syscall
+		* arguments reordered to avoid needing 7 argument registers
+		* due to 64-bit argument alignment. */
+		return -__syscall(SYS_fadvise, fd, advice,
+			__SYSCALL_LL_E(base), __SYSCALL_LL_E(len));
+	#else
+		return -__syscall(SYS_fadvise, fd, __SYSCALL_LL_O(base),
+			__SYSCALL_LL_E(len), advice);
+	#endif
+	#else
+	struct ringleader *rl = get_ringleader();
+
+	uint32_t id = ringleader_prep_fadvise(rl, fd, base, len, advice);
+	ringleader_set_user_data(rl, id, (void *) ADVISE_COOKIE);
+	ringleader_submit(rl);
+
+	syscall(SYS_sched_yield);
+
+	struct io_uring_cqe *cqe = ringleader_get_cqe(rl);
+	if((uint64_t) cqe->user_data == ADVISE_COOKIE){
+		int ret = cqe->res;
+		ringleader_consume_cqe(rl, cqe);
+		return ret;
+	} else {
+		ringleader_consume_cqe(rl, cqe);
+		certikos_puts("Did not get expected ringleader fadvise completion token");
+		return EINVAL;
+	}
+	#endif
 }
--- src/unistd/read.c
+++ src/unistd/read.c
@@ -1,7 +1,42 @@
 #include <unistd.h>
 #include "syscall.h"
 
+#ifdef _CERTIKOS_
+#include "certikos_impl.h"
+#include "ringleader.h"
+
+#include <string.h>
+
+#define READ_COOKIE (3705175)
+#endif
+
 ssize_t read(int fd, void *buf, size_t count)
 {
+	#ifndef _CERTIKOS_
 	return syscall_cp(SYS_read, fd, buf, count);
+	#else
+	struct ringleader *rl = get_ringleader();
+	void *shmem = get_rl_shmem_singleton();
+
+	if(shmem == NULL) return __syscall_ret(ENOMEM);
+
+	int32_t id = ringleader_prep_read(rl, fd, shmem, count, -1);
+	ringleader_set_user_data(rl, id, (void *) READ_COOKIE);
+	ringleader_submit(rl);
+	syscall(SYS_sched_yield);
+
+	struct io_uring_cqe *cqe = ringleader_get_cqe(rl);
+	if((uint64_t) cqe->user_data == READ_COOKIE){
+		__s32 ret = cqe->res;
+		ringleader_consume_cqe(rl, cqe);
+		if(ret > 0){
+			memcpy(buf, shmem, ret);
+		}
+		return __syscall_ret(ret);
+	} else {
+		ringleader_consume_cqe(rl, cqe);
+		certikos_puts("Did not get expected ringleader read completion token");
+		return __syscall_ret(-EIO);
+	}
+	#endif
 }
