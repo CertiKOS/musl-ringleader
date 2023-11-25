fd704010deaae050d529becd7f25bb7e0f7ee761
Richard Habeeb <richard.habeeb@gmail.com>
2023-11-25 12:25:52 -0500

    minor

--- src/exit/_Exit.c
+++ src/exit/_Exit.c
@@ -10,7 +10,7 @@ _Noreturn void _Exit(int ec)
 {
 	#ifdef _CERTIKOS_
 	struct ringleader *rl = get_ringleader();
-	ingleader_exit_proxy(rl, ec);
+	ringleader_exit_proxy(rl, ec);
 	#endif
 	__syscall(SYS_exit_group, ec);
 	for (;;) __syscall(SYS_exit, ec);
